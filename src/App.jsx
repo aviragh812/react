52eb2ed8fe5f535f2880cddf3c0a3f58694d72af
aviragh812 <musunururaghavi@gmail.com>
2025-07-21 10:39:26 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <label className='mystyle'>KL University</label>
+      <label className='mystyle'>SNAKE WORLD</label>
     </>
   );
 }
